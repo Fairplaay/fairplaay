3ba101b33257da536de2269fd8735e9a2468e074
Jesus Garcia <fairplaay16@gmail.com>
2019-12-15 11:35:26 -0400

    Updates

---
+++ precache-manifest.44ebc257ba6d81271c30facc5ce4c4ce.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0381d8574c6b64b01cf555a505f8c218",
+    "url": "/fairplaay/index.html"
+  },
+  {
+    "revision": "a385bdb172b82cf3b00e",
+    "url": "/fairplaay/static/css/main.ebef639e.chunk.css"
+  },
+  {
+    "revision": "fc97698767aa9a90d5d4",
+    "url": "/fairplaay/static/js/2.f117b7e9.chunk.js"
+  },
+  {
+    "revision": "b2f3f3632aa8113739d5f40daae9c618",
+    "url": "/fairplaay/static/js/2.f117b7e9.chunk.js.LICENSE"
+  },
+  {
+    "revision": "a385bdb172b82cf3b00e",
+    "url": "/fairplaay/static/js/main.e2e915b4.chunk.js"
+  },
+  {
+    "revision": "7d2bbd1f6486882cbfba",
+    "url": "/fairplaay/static/js/runtime-main.7c959fca.js"
+  },
+  {
+    "revision": "c6aaf6ba81535d6d83b5f581cbaa721c",
+    "url": "/fairplaay/static/media/fork.c6aaf6ba.svg"
+  },
+  {
+    "revision": "037d830416495def72b7881024c14b7b",
+    "url": "/fairplaay/static/media/roboto-v18-latin-700.037d8304.woff2"
+  },
+  {
+    "revision": "376e0950b361fbd3b09508031f498de5",
+    "url": "/fairplaay/static/media/roboto-v18-latin-700.376e0950.eot"
+  },
+  {
+    "revision": "57888be7f3e68a7050452ea3157cf4de",
+    "url": "/fairplaay/static/media/roboto-v18-latin-700.57888be7.svg"
+  },
+  {
+    "revision": "cae5027f600d2a0d88ac309655618e31",
+    "url": "/fairplaay/static/media/roboto-v18-latin-700.cae5027f.ttf"
+  },
+  {
+    "revision": "cf6613d1adf490972c557a8e318e0868",
+    "url": "/fairplaay/static/media/roboto-v18-latin-700.cf6613d1.woff"
+  },
+  {
+    "revision": "372d0cc3288fe8e97df49742baefce90",
+    "url": "/fairplaay/static/media/roboto-v18-latin-regular.372d0cc3.ttf"
+  },
+  {
+    "revision": "5d4aeb4e5f5ef754e307d7ffaef688bd",
+    "url": "/fairplaay/static/media/roboto-v18-latin-regular.5d4aeb4e.woff2"
+  },
+  {
+    "revision": "68889c246da2739681c1065d15a1ab0b",
+    "url": "/fairplaay/static/media/roboto-v18-latin-regular.68889c24.eot"
+  },
+  {
+    "revision": "8681f434273fd6a267b1a16a035c5f79",
+    "url": "/fairplaay/static/media/roboto-v18-latin-regular.8681f434.svg"
+  },
+  {
+    "revision": "bafb105baeb22d965c70fe52ba6b49d9",
+    "url": "/fairplaay/static/media/roboto-v18-latin-regular.bafb105b.woff"
+  },
+  {
+    "revision": "fd15fe26eeb77c73af91f4af1ffabd6f",
+    "url": "/fairplaay/static/media/star.fd15fe26.svg"
+  }
+]);
